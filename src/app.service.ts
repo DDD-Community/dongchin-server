f89644440ebec40689ee98829a9bbeda7465f9ff
TAEYONG KIM <86272688+YongsHub@users.noreply.github.com>
2022-05-25 23:40:03 +0900

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello This is CI/CD RDS TEST!';
+    return 'Hello This is CI/CD RDS TEST!!';
   }
 }
