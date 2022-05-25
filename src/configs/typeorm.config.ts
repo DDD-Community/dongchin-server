9f2b15c5b529c61d18ea7537059f5ffdec2e472c
김태용 <amico741@naver.com>
2022-05-25 19:14:50 +0900

    hotfix

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,11 +3,11 @@ import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 //typeORM 일단 Local에서 생성
 export const typeORMConfig : TypeOrmModuleOptions = {
     type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true
 }
