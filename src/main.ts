c69ae7c39c49463cbc01515a91e68ca8ff82111a
김태용 <amico741@naver.com>
2022-07-09 20:32:38 +0900

    Feat: html 파일 render
    for test

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule } from '@nestjs/swagger';
-import { env } from 'process';
+import { join } from 'path';
 import { BaseAPIDocumentation } from './api/base.document';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   /* mount Swagger API Documentation */
   const documentOptions = new BaseAPIDocumentation().initializeOptions();
   const document = SwaggerModule.createDocument(app, documentOptions);
   SwaggerModule.setup('api/v1/docs', app, document);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(3000);
 }
 bootstrap();
