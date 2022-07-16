316d8ad32192697986b682351fbc1cd16d88363d
김태용 <amico741@naver.com>
2022-07-16 19:27:16 +0900

    Feat: nickname 생성 시, 기본 보관함 생성

--- src/repository/nickname.repository.ts
+++ src/repository/nickname.repository.ts
@@ -3,21 +3,26 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Config } from '../nickname/constant/nickName.config';
 import { NickNameCredentialDto } from '../nickname/dto/nickname-credential.dto';
 import { Nickname } from '../entity/nickname.entity';
+import { StorageRepository } from './storage.repository';
 
 @EntityRepository(Nickname)
 export class NicknameRepository extends Repository<Nickname> {
   async createNickName(
     nicknameCredentialDto: NickNameCredentialDto,
+    storageRepository: StorageRepository,
   ): Promise<{ statusCode: number; ok: boolean; id?: number; error?: string }> {
     // 닉네임 생성 Function
     const { nickName } = nicknameCredentialDto;
-    const user = this.create({ nickName });
+    const storage = storageRepository.create({ name: '기본 보관함' });
+    const nickname = this.create({ nickName });
+    storage.nickname = nickname;
     try {
       // 닉네임 중복되지 않는다면
-      await this.save(user);
-      Logger.verbose('user', JSON.stringify(user));
+      await this.save(nickname);
+      await storageRepository.save(storage);
+      Logger.verbose('user', JSON.stringify(nickname));
       return Object.assign({
-        data: user,
+        data: nickname,
         statusCode: 201,
         ok: true,
         message: '닉네임이 저장되었습니다.',
