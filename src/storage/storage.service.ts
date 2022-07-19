80dcd40000ea9cb2439806c209c60eafefc9a631
TAEYONG KIM <86272688+YongsHub@users.noreply.github.com>
2022-07-19 20:47:11 +0900

    Refactor: 코드 정리 (#59)

--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { NicknameRepository } from 'src/repository/nickname.repository';
 import { StorageRepository } from 'src/repository/storage.repository';
