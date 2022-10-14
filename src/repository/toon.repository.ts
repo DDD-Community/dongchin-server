d68634b3ca0f0b15b8f08cd7096ecb7de4967f04
김태용 <amico741@naver.com>
2022-10-15 02:04:01 +0900

    Fix: 충돌 해결

--- src/repository/toon.repository.ts
+++ src/repository/toon.repository.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Logger, NotFoundException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { ToonDto } from '../toon/dto/toon-create.dto';
 import { Toon } from '../entity/toon.entity';
@@ -13,10 +13,6 @@ import {
 } from 'src/toon/config/type.config';
 import { Nickname } from 'src/entity/nickname.entity';
 
-export interface ToonFindAllOptions {
-  input?: string;
-  tagIds?: number[];
-}
 @EntityRepository(Toon)
 export class ToonRepository extends Repository<Toon> {
   private response: CommonResponseDto;
@@ -262,19 +258,5 @@ export class ToonRepository extends Repository<Toon> {
       }
     }
     return storageIds;
-  }
-
-  async findAll(options: ToonFindAllOptions = {}) {
-    const { input, tagIds } = options;
-    console.log(input);
-    console.log(tagIds);
-    const qb = this.createQueryBuilder('toon').leftJoinAndSelect(
-      'toon.tag',
-      'tag',
-    );
-    const tag = [1];
-    qb.andWhere('tag.id IN (:...tag)', { tag });
-    const [items, total] = await qb.getManyAndCount();
-    return { items, total };
   }
 }
